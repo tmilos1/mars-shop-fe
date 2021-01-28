cb8365ce393f4c1a10add25cf8a69d4153922796
Milos Tijanic <tmilos1@gmail.com>
2021-01-28 12:24:43 +0100

    Uklonjen redux

--- src/index.js
+++ src/index.js
@@ -3,18 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-import reducer from './store/reducer'
-
-const store = createStore(reducer)
-
-
 ReactDOM.render(
     <React.StrictMode>
-        <Provider store={store}>
-            <App />
-        </Provider>
+        <App />
     </React.StrictMode>,
     document.getElementById('root')
 );
--- src/store/sagas/products.js
+++
@@ -1,9 +0,0 @@
-import { put } from 'redux-saga/effects'
-import * as actionTypes from '../actions'
-
-function* fetchProducts(action) {
-    // yield axios ...
-    put({
-        action: actionTypes.FINISH_PRODUCT_FETCHING
-    })
-}
