3748e880916788031f65b3183560f641f8519b2a
Milos Tijanic <tmilos1@gmail.com>
2021-03-04 14:30:05 +0100

    prikaz korpe iz API-ja

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,4 +1,5 @@
 import { makeStyles } from '@material-ui/core/styles'
+import { useQuery } from 'react-query'
 
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
@@ -13,14 +14,15 @@ import RemoveIcon from '@material-ui/icons/Remove'
 const useStyles = makeStyles((theme) => ({
     root: {
         margin: theme.spacing(1),
-    },    
+    },
     listItemSlika: {
         margin: theme.spacing(1),
-        minWidth: "120px",
+        width: "120px",
     },
     listItemNaziv: {
         margin: theme.spacing(1),
         minWidth: "200px",
+        width: "200px",
     },
     listItemCena: {
         margin: theme.spacing(1),
@@ -34,66 +36,76 @@ const useStyles = makeStyles((theme) => ({
     },
     listItemIznos: {
         margin: theme.spacing(1),
-        minWidth: "80px",
-        textAlign: "right"
+        width: "80px",
+        textAlign: "right",
+        [theme.breakpoints.down('xs')]: {
+            display: 'none'
+        }        
     },
 }))
-
-
-
-const korpa = [
-    {
-        productId: '',
-        naziv: '',
-        cena: '',
-        kolicina: '',
-    }
-]
-// {process.env.REACT_APP_API_ROOT + "/slike/proizvodi/vece/" + props.product.productId + ".jpg"}
 
 export default function Cart(props) {
 
     const classes = useStyles()
 
+    const fetchCart = () => fetch(
+        process.env.REACT_APP_API_ROOT + "/cart/" + 1234567890
+    ).then((res) => res.json())
+
+    const { data: products, isSuccess } = useQuery(["cartData"], () => fetchCart())
+
+    let productsLen = 0
+    if (isSuccess) {
+        productsLen = products.length
+    }
+
     return (
         <List className={classes.root}>
-            <ListItem alignItems="flex-start">
-                <ListItemAvatar className={classes.listItemSlika}>
-                    <img src={process.env.REACT_APP_API_ROOT + "/slike/proizvodi/manje/26163.jpg"} width="110px" />
-                </ListItemAvatar>
-                <ListItemText
-                    primary={<>
-                        <p>POSUDA ZA ŠEĆER</p>
-                        <p>34323</p>
-                    </>}
-                    className={classes.listItemNaziv}
-                />
-                <ListItemText
-                    primary={<>
-                        <p>200 din</p>
-                    </>}
-                    className={classes.listItemCena}
-                />
-                <ListItemText
-                    primary={<>
-                        <IconButton color="primary">
-                            <RemoveIcon />
-                        </IconButton>
-                    3
-                    <IconButton color="primary">
-                            <AddIcon />
-                        </IconButton>
-                    </>}
-                    className={classes.listItemKolicina}
-                />
-                <ListItemText
-                    primary={<>
-                        <p>200 din</p>
-                    </>}
-                    className={classes.listItemIznos}
-                />
-            </ListItem>
-            <Divider />
+            {isSuccess &&
+                products.map((product, i) => (
+                    <>
+                        <ListItem alignItems="flex-start">
+                            <ListItemAvatar className={classes.listItemSlika}>
+                                <img src={process.env.REACT_APP_API_ROOT + "/slike/proizvodi/manje/" + product.productId + ".jpg"} width="110px" />
+                            </ListItemAvatar>
+                            <ListItemText
+                                primary={product.naziv}
+                                secondary={<>
+                                    <p>{"Šifra: " + product.productId}</p>
+                                </>}
+                                className={classes.listItemNaziv}
+                            />
+                            <ListItemText
+                                primary={<>
+                                    <p>{product.cena} din</p>
+                                </>}
+                                className={classes.listItemCena}
+                            />
+                            <ListItemText
+                                primary={<>
+                                    <IconButton color="primary">
+                                        <RemoveIcon />
+                                    </IconButton>
+                                {product.kolicina}
+                                <IconButton color="primary">
+                                        <AddIcon />
+                                    </IconButton>
+                                </>}
+                                className={classes.listItemKolicina}
+                            />
+                            <ListItemText
+                                primary={<>
+                                    <p>{product.iznos} din</p>
+                                </>}
+                                className={classes.listItemIznos}
+                            />
+                        </ListItem>
+                        {productsLen !== i + 1 &&
+                            <Divider />
+                        }
+                    </>
+                ))
+            }
         </List>
     )
 }
